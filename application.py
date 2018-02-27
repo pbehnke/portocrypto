cd2770dd1eec3097ecc7de7e4d41302a7df30c1b
Matej Grahovac <admin@matejgrahovac.de>
2018-02-27 21:01:57 +0100

    -

--- application.py
+++ application.py
@@ -8,6 +8,4 @@ def make_shell_context():
     return {'db': db, 'User': User, 'Transactions': Transactions, 'Coins': Coins}
 
 if __name__ == "__main__":
-	# port = int(os.environ.get("PORT", 5000))
-    application.debug = True
-	application.run(host='0.0.0.0')
+    application.run()
